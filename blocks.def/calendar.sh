5af54678229b1ac3a63342a93602b5c379175538
Sebastian Zehner <meinlifestylebusiness@gmail.com>
2025-07-23 11:22:29 -0300

    change date and time format

--- blocks.def/calendar.sh
+++ blocks.def/calendar.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-ICON="\x0b󰃰 \x0b"
-printf "$ICON%s" "$(date '+%a, %b %d, %R')"
+ICONc="\x0b󱛡 \x0b"
+#ICONc="\x0b󱨰 \x0b"
+#ICONt="\x0b \x0b"
+printf "$ICONc%s $ICONt%s" "$(date '+%d %b')" "$(date +%R)"
